8d7e871691762ea4fc913198964fe645668f0e1f
Simon Parsons <simon@mmaraboustork.co.uk>
2019-12-21 09:30:13 +0000

    initial commit

---
+++ test-data-generator/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace test_data_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quotes = File.ReadAllLines(@"..\..\..\input\quotes.txt");
+            var data = quotes.Select(l => new
+            {
+                quote = l.Split('|')[0],
+                author = l.Split('|')[1]
+            })
+            .ToList();
+
+            data.ForEach(d =>
+            {
+                Console.WriteLine(d);
+                File.WriteAllText($"..\\..\\..\\output\\{Guid.NewGuid().ToString()}.json", JsonConvert.SerializeObject(d));
+            });
+        }
+    }
+}
